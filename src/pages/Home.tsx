5116d9c30f5000cabbe5bb4fd1398007bb62d91c
vilayat571 <vilayat571@gmail.com>
2026-01-26 17:23:32 +0800

    feat: add products to prod branche

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,17 @@ import Layout from "../layout/Layout";
 import StatsSection from "../components/Home/StatsSection";
 import WhyChooseUs from "../components/Home/WhyChooseUs";
 import Intro from "../components/Home/Intro";
+import Products from "../components/Home/Products";
 
 const Home: React.FC = () => {
   return (
     <Layout>
-
-      
-      <Intro />
-      <StatsSection />
-      <WhyChooseUs />
+      <div className=" pb-20 bg-white">
+        <Intro />
+        <StatsSection />
+        <Products />
+        <WhyChooseUs />
+      </div>
     </Layout>
   );
 };
