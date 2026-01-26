43b5da4ce7037aee222963df99f238f9d02342c6
vilayat571 <vilayat571@gmail.com>
2026-01-26 17:39:25 +0800

    feat: add simple coming soon design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import Layout from "../layout/Layout";
 const Home: React.FC = () => {
   return (
     <Layout>
-  <div className="">
-    Coming soon
+  <div className="text-8xl flex items-center justify-center bg-[#101010] text-white h-screen w-full">
+     Tezliklə...
   </div>
     </Layout>
   );
