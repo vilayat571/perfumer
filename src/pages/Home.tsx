d1b3af087ade61a938f77f064e3d389e434c3d53
vilayat571 <vilayat571@gmail.com>
2026-01-26 17:30:37 +0800

    feat: add coming soon test text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Layout from "../layout/Layout";
-import StatsSection from "../components/Home/StatsSection";
-import Products from "../components/Home/Products";
-import WhyChooseUs from "../components/Home/WhyChooseUs";
-import Intro from "../components/Home/Intro";
+
 
 const Home: React.FC = () => {
   return (
     <Layout>
-      <div className=" pb-20 bg-white">
-        <Intro />
-        <StatsSection />
-        <Products />
-        <WhyChooseUs />
-      </div>
+  <div>
+    Coming soon
+  </div>
     </Layout>
   );
 };
