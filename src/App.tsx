3422669524c9bc1307efbc57aad538fe3e4ef17f
vilayat571 <vilayat571@gmail.com>
2026-02-16 07:48:35 +0800

    feat: add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import CreatePerfume from './pages/CreatePerfume'
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePerfume from "./pages/CreatePerfume";
+import { Analytics } from "@vercel/analytics/react"; // Changed from /next to /react
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create-perfume" element={<CreatePerfume />} />
+      <Analytics />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
